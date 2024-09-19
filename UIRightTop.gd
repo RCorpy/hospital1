ba4800373db6f2f38e76bc2b81f618b5fdc0b772
RCorpy <ramon.navm@gmail.com>
2024-09-20 00:08:02 +0200

    more stuff

--- UIRightTop.gd
+++ UIRightTop.gd
@@ -22,3 +22,16 @@ func go_back_to_game():
 
 func _on_button_back_pressed():
 	go_back_to_game()
+
+
+func _on_back_button_pressed():
+	$SettingsMenu/MainSettings.visible = true
+	$SettingsMenu/Option1.visible = false
+
+
+func _on_option_1_button_pressed():
+	$SettingsMenu/MainSettings.visible = false
+	$SettingsMenu/Option1.visible = true
+
+func _on_volume_value_changed(value):
+	pass # Replace with function body.
